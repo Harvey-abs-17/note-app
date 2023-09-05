c868717cf5b82c0438408ddb024bb0ee36ddeab9
Hadi <haadiabbasii17@gmail.com>
2023-09-05 13:35:47 +0330

    Note fragment ui completed.

--- app/src/main/java/com/example/noteapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/ui/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.noteapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.noteapp.R
 import com.example.noteapp.databinding.ActivityMainBinding
+import com.example.noteapp.utils.showView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,6 +26,24 @@ class MainActivity : AppCompatActivity() {
 
         binding.apply {
             bottomNav.background = null
+            bottomNav.setupWithNavController(navController)
+            // manage visibility of bottom app bar and fab
+            navController.addOnDestinationChangedListener { _, destination, _ ->
+                when(destination.id){
+                    R.id.noteFragment -> {
+                        bottomAppBar.showView(false)
+                        addNoteBtn.showView(false)
+                    }
+                    R.id.homeFragment ->{
+                        bottomAppBar.showView(true)
+                        addNoteBtn.showView(true)
+                    }
+                }
+            }
+            //navigate to note fragment
+            addNoteBtn.setOnClickListener {
+                navController.navigate(R.id.noteFragment)
+            }
         }
     }
 }
